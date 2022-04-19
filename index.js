de50d6b839d39c1811006d5162d2ce4487ad2e9c
melodoc <liubov_rz@yahoo.com>
2022-04-19 18:41:44 +0400

    feat: add input manage

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// open-close implementation
 const openProfileButton = document.querySelector('.profile__button_action_edit');
 const closeProfileButton = document.querySelector('.popup__button_action_close');
 const popup = document.querySelector('.popup');
@@ -15,3 +16,21 @@ function closeProfile() {
 
 openProfileButton.addEventListener('click', openProfile);
 closeProfileButton.addEventListener('click', closeProfile);
+
+// input manage
+
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__text');
+const popUpNameInput = document.querySelector('.popup__input_type_name');
+const popUpAboutInput = document.querySelector('.popup__input_type_about');
+
+function setInputValue(input, value) {
+    input.setAttribute('value', value);
+}
+
+function setInitialFormData() {
+    setInputValue(popUpNameInput, profileName.textContent);
+    setInputValue(popUpAboutInput, profileAbout.textContent);
+}
+
+setInitialFormData();
