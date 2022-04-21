7c1d5428dd6dae44c8796a15e880d2b5aa06a637
melodoc <liubov_rz@yahoo.com>
2022-04-22 01:10:28 +0400

    hotfix: fix js according to the review

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,30 @@
-// open-close implementation
+// open-close elements
 const openProfileButton = document.querySelector('.profile__button_action_edit');
 const closeProfileButton = document.querySelector('.popup__button_action_close');
 const popup = document.querySelector('.popup');
 const popupOpenedClass = 'popup_opened';
+// input elements
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__text');
+const popUpNameInput = document.querySelector('.popup__input_type_name');
+const popUpAboutInput = document.querySelector('.popup__input_type_about');
+// submit form element
+const profileForm = document.querySelector('.popup__form');
 
+// set profile form data functions
+function setInputValue(input, value) {
+    input.setAttribute('value', value);
+}
+
+function setProfileFormData() {
+    setInputValue(popUpNameInput, profileName.textContent);
+    setInputValue(popUpAboutInput, profileAbout.textContent);
+}
+
+// open-close implementation
 function openProfile() {
-    if (!popup.classList.contains(popupOpenedClass)) {
-        popup.classList.add(popupOpenedClass);
-    }
+    setProfileFormData();
+    popup.classList.add(popupOpenedClass);
 }
 
 function closeProfile() {
@@ -17,33 +34,12 @@ function closeProfile() {
 openProfileButton.addEventListener('click', openProfile);
 closeProfileButton.addEventListener('click', closeProfile);
 
-// input manage
-
-const profileName = document.querySelector('.profile__name');
-const profileAbout = document.querySelector('.profile__text');
-const popUpNameInput = document.querySelector('.popup__input_type_name');
-const popUpAboutInput = document.querySelector('.popup__input_type_about');
-
-function setInputValue(input, value) {
-    input.setAttribute('value', value);
-}
-
-function setInitialFormData() {
-    setInputValue(popUpNameInput, profileName.textContent);
-    setInputValue(popUpAboutInput, profileAbout.textContent);
-}
-
-setInitialFormData();
-
-// submit form
-
-const formElement = document.querySelector('.popup__form');
-
-function formSubmitHandler(evt) {
+// submit form manage
+function handleProfileFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = popUpNameInput.value;
     profileAbout.textContent = popUpAboutInput.value;
     closeProfile();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+profileForm.addEventListener('submit', handleProfileFormSubmit);
