874cc424ab1ee058840449aaaacf7bb624728200
melodoc <liubov_rz@yahoo.com>
2022-06-21 01:19:01 +0400

    feat: Add PopupWithForm class

--- scripts/PopupWithForm.js
+++ scripts/PopupWithForm.js
@@ -1,7 +1,37 @@
 import { Popup } from './Popup.js';
-
+/** *
+ * PopupWithForm is responsible for popup with form opening and closing
+ *
+ * @param popupSelector - Popup with form selector
+ * @param submitFormHandler - submitFormHandler
+ */
 export class PopupWithForm extends Popup {
-    constructor(popupSelector) {
+    constructor(popupSelector, submitFormHandler) {
         super(popupSelector);
+        this._form = document.querySelector(this.popupSelector);
+        this._inputList = Array.from(this._form.querySelectorAll('.popup__input'));
+        this._submitBtn = this._form.querySelector('.popup__button_action_submit');
+        this._submitFormHandler = submitFormHandler;
+    }
+
+    _getInputValues() {
+        const inputValues = {};
+        this._inputList.forEach((input) => {
+            inputValues[input.name] = input.value;
+        });
+        return inputValues;
+    }
+
+    setEventListeners() {
+        super.setEventListeners();
+        this._form.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+            this._submitFormHandler();
+        });
+    }
+
+    close(element) {
+        super.close(element);
+        this._form.reset();
     }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@ import { FormValidator } from './FormValidator.js';
 import { Section } from './Section.js';
 import { Popup } from './Popup.js';
 import { PopupWithImage } from './PopupWithImage.js';
+import { PopupWithForm } from './PopupWithForm.js';
 
 // open-close profile elements
 const openProfileButton = document.querySelector('.profile__button_action_edit');
@@ -19,23 +20,12 @@ const addCardPopupUrlInput = document.querySelector('.popup__input_type_url');
 
 // submit form elements
 const profileForm = document.querySelector('#profile .popup__form');
-
 const addCardForm = document.querySelector('#add-card .popup__form');
 
 // card template
 const cardTemplate = document.querySelector('#card').content;
 const openAddCardButton = document.querySelector('.profile__button_action_add');
 const addCardPopup = document.querySelector('#add-card');
-
-// set profile form data functions
-function setInputValue(input, value) {
-    input.setAttribute('value', value);
-}
-
-function setProfileFormData() {
-    setInputValue(profilePopupNameInput, profileName.textContent);
-    setInputValue(profilePopupAboutInput, profileAbout.textContent);
-}
 
 const popup = new Popup(config.popupOpenedSelector);
 const cardPopup = new PopupWithImage('#zoom-img');
@@ -76,26 +66,22 @@ const enableValidation = (config) => {
 
 enableValidation(config);
 
-openProfileButton.addEventListener('click', () => {
-    setProfileFormData();
-    formValidators[profileForm.getAttribute('name')].resetValidation();
-    popup.open(profilePopup);
-});
-
-profileForm.addEventListener('submit', (evt) => {
-    evt.preventDefault();
+const profileFormPopup = new PopupWithForm('#profile .popup__form', () => {
     profileName.textContent = profilePopupNameInput.value;
     profileAbout.textContent = profilePopupAboutInput.value;
-    popup.close(evt.target.closest(config.popupOpenedSelector));
+    profileFormPopup.close(document.querySelector(config.popupOpenedSelector));
 });
 
-openAddCardButton.addEventListener('click', () => {
-    popup.open(addCardPopup);
+profileFormPopup.setEventListeners();
+
+openProfileButton.addEventListener('click', () => {
+    profilePopupNameInput.setAttribute('value', profileName.textContent);
+    profilePopupAboutInput.setAttribute('value', profileAbout.textContent);
+    formValidators[profileForm.getAttribute('name')].resetValidation();
+    profileFormPopup.open(profilePopup);
 });
 
-addCardForm.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-
+const addCardFormPopup = new PopupWithForm('#add-card .popup__form', () => {
     const cardData = [
         {
             name: addCardPopupTitleInput.value,
@@ -115,8 +101,13 @@ addCardForm.addEventListener('submit', (evt) => {
     );
 
     additionalCards.render();
-
     addCardForm.reset();
     formValidators[addCardForm.getAttribute('name')].resetValidation();
-    popup.close(evt.target.closest(config.popupOpenedSelector));
+    addCardFormPopup.close(document.querySelector(config.popupOpenedSelector));
+});
+
+addCardFormPopup.setEventListeners();
+
+openAddCardButton.addEventListener('click', () => {
+    addCardFormPopup.open(addCardPopup);
 });
