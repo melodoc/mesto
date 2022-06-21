14758b6d15bee4b15c5877e9060a9ec8068ffc56
melodoc <liubov_rz@yahoo.com>
2022-06-21 12:23:17 +0400

    fix: Enable webpack build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
-import { initialCards, config, cardSelectors } from './constants.js';
-import { Card } from './Card.js';
-import { FormValidator } from './FormValidator.js';
-import { Section } from './Section.js';
-import { Popup } from './Popup.js';
-import { PopupWithImage } from './PopupWithImage.js';
-import { PopupWithForm } from './PopupWithForm.js';
-import { UserInfo } from './UserInfo.js';
+import { initialCards, config, cardSelectors } from '../constants/constants.js';
+import { Card } from '../components/Card.js';
+import { FormValidator } from '../components/FormValidator.js';
+import { Section } from '../components/Section.js';
+import { Popup } from '../components/Popup.js';
+import { PopupWithImage } from '../components/PopupWithImage.js';
+import { PopupWithForm } from '../components/PopupWithForm.js';
+import { UserInfo } from '../components/UserInfo.js';
+import './index.css';
 
 // open-close profile elements
 const openProfileButton = document.querySelector('.profile__button_action_edit');
